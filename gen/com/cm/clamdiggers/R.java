e2e09bc576d8f24c1155de576c7879341f66b41d
Crystal McDonald <stalmcdonald@fullsail.edu>
2013-09-10 20:38:21 -0700

    Content Provider Implemented
    not functioning atm /  also added custom button

--- gen/com/cm/clamdiggers/R.java
+++ gen/com/cm/clamdiggers/R.java
@@ -12,8 +12,10 @@ public final class R {
     }
     public static final class drawable {
         public static final int bg=0x7f020000;
-        public static final int ic_launcher=0x7f020001;
-        public static final int splash_back=0x7f020002;
+        public static final int brownbutton=0x7f020001;
+        public static final int ic_launcher=0x7f020002;
+        public static final int splash_back=0x7f020003;
+        public static final int texturedbttn=0x7f020004;
     }
     public static final class id {
         public static final int bPrediction=0x7f070003;
