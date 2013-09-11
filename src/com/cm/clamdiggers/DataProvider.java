3585a1bc709d66379ee274fb96e2605c9b71fafa
Crystal McDonald <stalmcdonald@fullsail.edu>
2013-09-10 23:44:07 -0700

    created DbHelper Class
    with variables

--- src/com/cm/clamdiggers/DataProvider.java
+++ src/com/cm/clamdiggers/DataProvider.java
@@ -9,11 +9,37 @@ package com.cm.clamdiggers;
 
 import android.content.ContentProvider;
 import android.content.ContentValues;
+import android.content.UriMatcher;
 import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.net.Uri;
 
 public class DataProvider extends ContentProvider {
 
+	//create a private string based on what package is called
+	private static final String AUTH = "com.cm.clamdiggers.DataProvider";
+	//static variable for the whole application  path to the content provider
+	//uri is like a url (web address) to the database in the application
+	//creating private uri for app
+	public static final Uri TIDES_URI = Uri.parse("content://"+AUTH+"/"+DbHelper.TABLE_NAME);//change dbHelper to another class
+	
+	final static int TIDE = 1;
+	
+	//setting up variables for database
+	SQLiteDatabase db;
+	DbHelper dbhelper;//change class name
+	
+	//creating uri matcher
+	private final static UriMatcher uriMatcher;
+	
+	static
+	{
+		//initializes uriMatcher
+		uriMatcher = new UriMatcher(UriMatcher.NO_MATCH);
+		int code;
+		//adding uri to uriMatcher
+		uriMatcher.addURI(AUTH, DbHelper.TABLE_NAME, code);
+	}
 	@Override
 	public int delete(Uri arg0, String arg1, String[] arg2) {
 		// TODO Auto-generated method stub
---
+++ src/com/cm/clamdiggers/DbHelper.java
@@ -0,0 +1,5 @@
+package com.cm.clamdiggers;
+
+public class DbHelper {
+
+}
